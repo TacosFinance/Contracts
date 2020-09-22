e736082bd979f3fc80f2a8fa010f022f0b5d9b69
TacosFinance <71722574+TacosFinance@users.noreply.github.com>
2020-09-22 19:00:23 +0200

    Update and rename PickleToken.sol to TacosToken.sol

--- YieldFarming/TacosToken.sol
+++ YieldFarming/TacosToken.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-// PickleToken with Governance.
-contract PickleToken is ERC20("PickleToken", "PICKLE"), Ownable {
+// TacosToken with Governance.
+contract TacosToken is ERC20("TacosToken", "TACOS"), Ownable {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
