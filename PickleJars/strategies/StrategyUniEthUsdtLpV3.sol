1e0e69e306c4f37e656d09cd84493c579c15d97d
TacosFinance <71722574+TacosFinance@users.noreply.github.com>
2020-09-22 19:58:59 +0200

    Update StrategyUniEthUsdtLpV3.sol

--- PickleJars/strategies/StrategyUniEthUsdtLpV3.sol
+++ PickleJars/strategies/StrategyUniEthUsdtLpV3.sol
@@ -1167,8 +1167,8 @@ contract StrategyUniEthUsdtLpV3 {
     // stablecoins
     address public constant usdt = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
 
-    // pickle token
-    address public constant pickle = 0x429881672B9AE42b8EbA0E26cD9C73711b891Ca5;
+    // tacos token
+    address public constant tacos = 0x429881672B9AE42b8EbA0E26cD9C73711b891Ca5;
 
     // weth
     address public constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
@@ -1185,7 +1185,7 @@ contract StrategyUniEthUsdtLpV3 {
 
     // Fees ~4.5% in total
     // - 3%  performance fee
-    // - 1.5%   used to burn pickles
+    // - 1.5%   used to burn tacos
 
     // 3% of 98% = 2.94% of original 100%
     uint256 public performanceFee = 300;
@@ -1371,12 +1371,12 @@ contract StrategyUniEthUsdtLpV3 {
         // Swap half WETH for USDT
         uint256 _weth = IERC20(weth).balanceOf(address(this));
         if (_weth > 0) {
-            // Burn some pickles first
+            // Burn some tacos first
             uint256 _burnFee = _weth.mul(burnFee).div(burnMax);
-            _swap(weth, pickle, _burnFee);
-            IERC20(pickle).transfer(
+            _swap(weth, tacos, _burnFee);
+            IERC20(tacos).transfer(
                 burn,
-                IERC20(pickle).balanceOf(address(this))
+                IERC20(tacos).balanceOf(address(this))
             );
 
             _weth = _weth.sub(_burnFee);
